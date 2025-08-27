e76be1fba07b7fbdb29bf15589e97379a402d440
Raizkin <148555404+Raizkin@users.noreply.github.com>
2025-08-27 15:17:05 +0300

    zxzaxzzZx

--- src/js/number.js
+++ src/js/number.js
@@ -4,22 +4,30 @@ const form = document.querySelector('.number__form');
 const input = document.querySelector('.number__input');
 const message = document.querySelector('.number__subtitle-first');
 
+function newGame() {
+  secretNumber = Math.floor(Math.random() * 15) + 1; 
+  input.value = '';
+  setTimeout(() => {
+    message.textContent = 'Нова гра! Спробуйте вгадати число.';
+    message.style.color = 'black';
+  }, 2500); 
+}
+
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
-  const guess = Number(input.value); 
+  const guess = Number(input.value);
 
-  if (isNaN(guess)) {
-    message.textContent = 'Будь ласка, введіть число!';
+  if (isNaN(guess) || guess < 1 || guess > 15) {
+    message.textContent = 'Будь ласка, введіть число від 1 до 15!';
     message.style.color = 'red';
     return;
   }
 
   if (guess === secretNumber) {
-    message.textContent = `Вітаю, ви вгадали число! (${secretNumber})`;
+    message.textContent = `Вітаю, ви вгадали число! (${secretNumber}) 🎉`;
     message.style.color = 'green';
-
-    secretNumber = Math.floor(Math.random() * 100) + 1;
+    newGame(); 
   } else if (guess < secretNumber) {
     message.textContent = 'Загадане число більше!';
     message.style.color = 'orange';
@@ -27,6 +35,4 @@ form.addEventListener('submit', function (e) {
     message.textContent = 'Загадане число менше!';
     message.style.color = 'orange';
   }
-
-//   input.value = '';
 });
